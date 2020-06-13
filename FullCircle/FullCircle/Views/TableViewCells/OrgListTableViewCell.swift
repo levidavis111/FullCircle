cee182d5f197c5a49f42ef7da94e581c37e6595f
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 13:29:27 -0400

    adds subviews and constraints to ui objects in orgListTVCell

--- FullCircle/FullCircle/Views/TableViewCells/OrgListTableViewCell.swift
+++ FullCircle/FullCircle/Views/TableViewCells/OrgListTableViewCell.swift
@@ -29,6 +29,8 @@ class OrgListTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        addSubviews()
+        addConstraints()
     }
     
     required init?(coder: NSCoder) {

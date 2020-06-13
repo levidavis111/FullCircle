58c014e053da445e0fe0e30b63c747936044e434
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 12:39:42 -0400

    adds searchbar and orgListTableView to subview and creates constraints

--- FullCircle/FullCircle/Controllers/OrgListViewController+Constraints.swift
+++ FullCircle/FullCircle/Controllers/OrgListViewController+Constraints.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2020 FullCircle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension OrgListViewController {
+    func addSubviews() {
+        view.addSubview(searchBar)
+        view.addSubview(orgListTableView)
+    }
+    
+    func addConstraints() {
+        setSearchBarConstraints()
+        setOrgListTableViewConstraints()
+    }
+    
+    
+    private func setSearchBarConstraints() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            searchBar.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            searchBar.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchBar.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func setOrgListTableViewConstraints() {
+        orgListTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            orgListTableView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            orgListTableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            orgListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            orgListTableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+        ])
+    }
+    
+}

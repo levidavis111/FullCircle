4548f2c0baabb278c46cda6be9756cc0aa846c7a
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-12 21:31:22 -0400

    creates Organization.swift and Action.swift model files in Model directory

---
+++ FullCircle/FullCircle/Models/Action.swift
@@ -0,0 +1,13 @@
+//
+//  Action.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 6/12/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
+
+struct Action {
+    
+}
---
+++ FullCircle/FullCircle/Models/Organization.swift
@@ -0,0 +1,13 @@
+//
+//  Organization.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 6/12/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation
+
+struct Organization {
+    
+}

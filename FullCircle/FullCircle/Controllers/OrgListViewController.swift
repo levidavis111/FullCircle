5ea7bb1d0c96f853bff645f119daab9a8e4b9839
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 12:40:13 -0400

    adds objects and constraints to OrgListVC

--- FullCircle/FullCircle/Controllers/OrgListViewController.swift
+++ FullCircle/FullCircle/Controllers/OrgListViewController.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class OrgListViewController: UIViewController {
-
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        return searchBar
+    }()
+    
+    lazy var orgListTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        
+        addSubviews()
+        addConstraints()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

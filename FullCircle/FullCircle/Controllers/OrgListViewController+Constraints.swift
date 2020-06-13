a3d55e894fecbce982130199cd4e6ad587f2d6ea
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 12:29:39 -0400

    creates OrgListVC+Constraints.swift and OrgListVC+TableView.swift

---
+++ FullCircle/FullCircle/Controllers/OrgListViewController+Constraints.swift
@@ -0,0 +1,9 @@
+//
+//  OrgListViewController+Constraints.swift
+//  FullCircle
+//
+//  Created by Sunni Tang on 6/13/20.
+//  Copyright © 2020 FullCircle. All rights reserved.
+//
+
+import Foundation

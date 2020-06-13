ad4513ab2f99a097605c69d27e951ca475c5b062
levi davis <ldd235@gmail.com>
2020-06-13 17:51:09 -0400

    Adds icon image view and exclusion path. Sets rough ui constraints

--- FullCircle/FullCircle/Controllers/ActionDetailViewController.swift
+++ FullCircle/FullCircle/Controllers/ActionDetailViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ActionDetailViewController: UIViewController {
     
     var action: Action!
+    private let padding: CGFloat = 10.0
+    private lazy var imagePath = UIBezierPath(rect: activityTypeIconView.frame)
     
     private lazy var orgLogoView: UIImageView = {[weak self] in
         let imageView = UIImageView()
@@ -40,6 +42,7 @@ class ActionDetailViewController: UIViewController {
     private lazy var takeActionButton: UIButton = {
         let button = UIButton()
         button.setTitle("Take Action", for: .normal)
+        button.backgroundColor = .green
         button.addTarget(self, action: #selector(takeActionButtonPressed), for: .touchUpInside)
         return button
     }()
@@ -48,10 +51,23 @@ class ActionDetailViewController: UIViewController {
         super.viewDidLoad()
         addSubviews()
         constrainSubviews()
+        setExclusionPath()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setExclusionPath()
     }
     
     @objc private func takeActionButtonPressed() {
         
+    }
+    
+    private func setExclusionPath() {
+        DispatchQueue.main.async {[weak self] in
+            self?.descriptionTextView.textContainer.exclusionPaths = [self?.imagepath]
+            self?.view.layoutIfNeeded()
+        }
     }
     
     private func addSubviews() {
@@ -66,22 +82,43 @@ class ActionDetailViewController: UIViewController {
         constrainOrgLogoView()
         constrainOrgNameLabel()
         constrainDescriptionTextView()
+        constrainActivityTypeIconView()
         constrainTakeActionButton()
     }
     
     private func constrainOrgLogoView() {
         orgLogoView.translatesAutoresizingMaskIntoConstraints = false
+        [orgLogoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+         orgLogoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+         orgLogoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding), orgLogoView.heightAnchor.constraint(equalToConstant: 150)].forEach {$0.isActive = true}
+        
     }
     private func constrainOrgNameLabel() {
-        orgLogoView.translatesAutoresizingMaskIntoConstraints = false
+        orgNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        [orgNameLabel.topAnchor.constraint(equalTo: orgLogoView.bottomAnchor, constant: padding),
+         orgNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)].forEach {$0.isActive = true}
     }
     
     private func constrainDescriptionTextView() {
-        orgLogoView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        [descriptionTextView.topAnchor.constraint(equalTo: orgNameLabel.bottomAnchor, constant: padding),
+         descriptionTextView.bottomAnchor.constraint(equalTo: takeActionButton.topAnchor, constant: -padding),
+         descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding), descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding)].forEach {$0.isActive = true}
+    }
+    
+    private func constrainActivityTypeIconView() {
+        activityTypeIconView.translatesAutoresizingMaskIntoConstraints = false
+        [activityTypeIconView.topAnchor.constraint(equalTo: descriptionTextView.topAnchor),
+         activityTypeIconView.trailingAnchor.constraint(equalTo: descriptionTextView.trailingAnchor),
+         activityTypeIconView.heightAnchor.constraint(equalToConstant: 30)].forEach {$0.isActive = true}
     }
     
     private func constrainTakeActionButton() {
-        orgLogoView.translatesAutoresizingMaskIntoConstraints = false
+        takeActionButton.translatesAutoresizingMaskIntoConstraints = false
+        [takeActionButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: padding),
+         takeActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: (2 * padding)),
+         takeActionButton.heightAnchor.constraint(equalToConstant: 30),
+         takeActionButton.widthAnchor.constraint(equalToConstant: 60)].forEach {$0.isActive = true}
     }
 
 }

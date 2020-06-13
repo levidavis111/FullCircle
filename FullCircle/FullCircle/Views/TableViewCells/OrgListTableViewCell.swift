a27451fec660acf216d3cce7125e15e123a4abf3
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 13:06:16 -0400

    creates ui objects and initalizers and cell lifecycle methods in OrgListTVCell

--- FullCircle/FullCircle/Views/TableViewCells/OrgListTableViewCell.swift
+++ FullCircle/FullCircle/Views/TableViewCells/OrgListTableViewCell.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2020 FullCircle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class OrgListTableViewCell: UITableViewCell {
+    
+    //MARK: - UI Objects
+    let orgNameLabel = UILabel()
+    let orgBoroughLabel = UILabel()
+    let orgTypeLabel = UILabel()
+    
+    lazy var orgLogoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    //TODO: Add Follow button for users
+    
+    //MARK: - Initializers
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Cell Lifecycle Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+}

af74d9e071754916cc452b86a8a7977e55f4b3de
levi davis <ldd235@gmail.com>
2020-06-14 13:09:33 -0400

    Adds take action nows for all actions.

--- FullCircle/FullCircle/Controllers/ActionDetailViewController.swift
+++ FullCircle/FullCircle/Controllers/ActionDetailViewController.swift
@@ -15,11 +15,11 @@ class ActionDetailViewController: UIViewController {
     
     var action: Action!
     private let padding: CGFloat = 10.0
-   
-
+    
+    
     let lat = 40.6739
     let long = -73.9701
-
+    
     let initialLocation = CLLocation(latitude: 40.6739, longitude: -73.9701)
     let searchRadius: Double = 2000
     
@@ -79,7 +79,7 @@ class ActionDetailViewController: UIViewController {
         button.addTarget(self, action: #selector(dismissButtonPressed), for: .touchUpInside)
         return button
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
@@ -90,146 +90,172 @@ class ActionDetailViewController: UIViewController {
     }
     
     @objc private func takeActionButtonPressed() {
-        if action.organization.name == "Black Lives Matter" {
+        
+        switch action.actionCode {
+        case .protest:
             showMap()
-        } else if action.organization.name == "Color of Change" {
+        case .email:
             sendEmail()
-        } else {
+        case .phone:
+            callRep()
+        case .donate:
+            openDonatePage()
+        case .petition:
+            openPetitionPage()
+        default:
             let alert = UIAlertController(title: "Please Contact Us For More Info", message: "\(action.organization.email)\n\(action.organization.phone)", preferredStyle: .alert)
             let action = UIAlertAction(title: "DISMISS", style: .default, handler: nil)
             alert.addAction(action)
             DispatchQueue.main.async {[weak self] in
                 self?.present(alert, animated: true)
             }
-        }
-    }
-    
-    @objc private func dismissButtonPressed() {
-        mapView.isHidden = true
-        mapView.isUserInteractionEnabled = false
-        dismissButton.isUserInteractionEnabled = false
-        dismissButton.isHidden = true
-        navigationController?.navigationBar.isHidden = false
-        view.sendSubviewToBack(mapView)
-    }
-    
-    private func sendEmail() {
-        if MFMailComposeViewController.canSendMail() {
-            let mail = MFMailComposeViewController()
-            mail.mailComposeDelegate = self
-            mail.setToRecipients(["localrep@government.gov"])
-            mail.setMessageBody("<p>Dear local lawmaker: Please pledge your support for the new police reform bill</p>", isHTML: true)
 
-            present(mail, animated: true)
-        } else {
-            print("Could not send mail")
-            // show failure alert
         }
     }
-    
-    private func showMap() {
-        mapView.isHidden = false
-        mapView.isUserInteractionEnabled = true
-        dismissButton.isHidden = false
-        dismissButton.isUserInteractionEnabled = true
-        navigationController?.navigationBar.isHidden = true
-        view.bringSubviewToFront(mapView)
-    }
-    
-    private func setupDelegates() {
-        mapView.delegate = self
-        mapView.userTrackingMode = .follow
-       
-    }
-    
-    private func addSubviews() {
-        view.addSubview(orgLogoView)
-        view.addSubview(orgNameLabel)
-        view.addSubview(descriptionTextView)
-        descriptionTextView.addSubview(activityTypeIconView)
-        view.addSubview(takeActionButton)
-        view.addSubview(mapView)
-        mapView.addSubview(dismissButton)
-    }
-    
-    private func constrainSubviews() {
-        constrainOrgLogoView()
-        constrainOrgNameLabel()
-        constrainDescriptionTextView()
-        constrainActivityTypeIconView()
-        constrainTakeActionButton()
-        constrainMapView()
-        constrainDismissButton()
-    }
-    
-    private func constrainOrgLogoView() {
-        orgLogoView.translatesAutoresizingMaskIntoConstraints = false
-        [orgLogoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
-         orgLogoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
-         orgLogoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding), orgLogoView.heightAnchor.constraint(equalToConstant: 150)].forEach {$0.isActive = true}
+        
+        @objc private func dismissButtonPressed() {
+            mapView.isHidden = true
+            mapView.isUserInteractionEnabled = false
+            dismissButton.isUserInteractionEnabled = false
+            dismissButton.isHidden = true
+            navigationController?.navigationBar.isHidden = false
+            view.sendSubviewToBack(mapView)
+        }
+        
+        private func openDonatePage () {
+            if let url = URL(string: "\(action.actionURL)") {
+                UIApplication.shared.open(url)
+            }
+        }
+        
+        private func callRep() {
+            guard let number = URL(string: "tel://" + action.actionURL) else { return }
+            UIApplication.shared.open(number)
+        }
+        
+        private func openPetitionPage() {
+            if let url = URL(string: "\(action.actionURL)") {
+                UIApplication.shared.open(url)
+            }
+        }
+        
+        private func sendEmail() {
+            if MFMailComposeViewController.canSendMail() {
+                let mail = MFMailComposeViewController()
+                mail.mailComposeDelegate = self
+                mail.setToRecipients(["localrep@government.gov"])
+                mail.setMessageBody("<p>Dear local lawmaker: Please pledge your support for the new police reform bill</p>", isHTML: true)
+                
+                present(mail, animated: true)
+            } else {
+                print("Could not send mail")
+                // show failure alert
+            }
+        }
+        
+        private func showMap() {
+            mapView.isHidden = false
+            mapView.isUserInteractionEnabled = true
+            dismissButton.isHidden = false
+            dismissButton.isUserInteractionEnabled = true
+            navigationController?.navigationBar.isHidden = true
+            view.bringSubviewToFront(mapView)
+        }
+        
+        private func setupDelegates() {
+            mapView.delegate = self
+            mapView.userTrackingMode = .follow
+            
+        }
+        
+        private func addSubviews() {
+            view.addSubview(orgLogoView)
+            view.addSubview(orgNameLabel)
+            view.addSubview(descriptionTextView)
+            descriptionTextView.addSubview(activityTypeIconView)
+            view.addSubview(takeActionButton)
+            view.addSubview(mapView)
+            mapView.addSubview(dismissButton)
+        }
+        
+        private func constrainSubviews() {
+            constrainOrgLogoView()
+            constrainOrgNameLabel()
+            constrainDescriptionTextView()
+            constrainActivityTypeIconView()
+            constrainTakeActionButton()
+            constrainMapView()
+            constrainDismissButton()
+        }
+        
+        private func constrainOrgLogoView() {
+            orgLogoView.translatesAutoresizingMaskIntoConstraints = false
+            [orgLogoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+             orgLogoView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+             orgLogoView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding), orgLogoView.heightAnchor.constraint(equalToConstant: 150)].forEach {$0.isActive = true}
+            
+        }
+        private func constrainOrgNameLabel() {
+            orgNameLabel.translatesAutoresizingMaskIntoConstraints = false
+            [orgNameLabel.topAnchor.constraint(equalTo: orgLogoView.bottomAnchor, constant: padding),
+             orgNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)].forEach {$0.isActive = true}
+        }
+        
+        private func constrainDescriptionTextView() {
+            descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+            [descriptionTextView.topAnchor.constraint(equalTo: orgNameLabel.bottomAnchor, constant: padding),
+             descriptionTextView.bottomAnchor.constraint(equalTo: takeActionButton.topAnchor, constant: -padding),
+             descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding), descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding)].forEach {$0.isActive = true}
+        }
+        
+        private func constrainActivityTypeIconView() {
+            activityTypeIconView.translatesAutoresizingMaskIntoConstraints = false
+            [activityTypeIconView.topAnchor.constraint(equalTo: descriptionTextView.topAnchor),
+             activityTypeIconView.trailingAnchor.constraint(equalTo: descriptionTextView.trailingAnchor),
+             activityTypeIconView.heightAnchor.constraint(equalToConstant: 30)].forEach {$0.isActive = true}
+        }
+        
+        private func constrainTakeActionButton() {
+            takeActionButton.translatesAutoresizingMaskIntoConstraints = false
+            [takeActionButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: padding),
+             takeActionButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+             takeActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: (-4 * padding)),
+             takeActionButton.heightAnchor.constraint(equalToConstant: 30),
+             takeActionButton.widthAnchor.constraint(equalToConstant: 180)].forEach {$0.isActive = true}
+        }
+        
+        private func constrainMapView() {
+            mapView.translatesAutoresizingMaskIntoConstraints = false
+            [mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+             mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+             mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+             mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)].forEach {$0.isActive = true}
+        }
+        
+        private func constrainDismissButton() {
+            dismissButton.translatesAutoresizingMaskIntoConstraints = false
+            [dismissButton.leadingAnchor.constraint(equalTo: mapView.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+             dismissButton.bottomAnchor.constraint(equalTo: mapView.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+             dismissButton.heightAnchor.constraint(equalToConstant: 20),
+             dismissButton.widthAnchor.constraint(equalToConstant: 80)].forEach {$0.isActive = true}
+        }
         
     }
-    private func constrainOrgNameLabel() {
-        orgNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        [orgNameLabel.topAnchor.constraint(equalTo: orgLogoView.bottomAnchor, constant: padding),
-         orgNameLabel.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor)].forEach {$0.isActive = true}
-    }
     
-    private func constrainDescriptionTextView() {
-        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
-        [descriptionTextView.topAnchor.constraint(equalTo: orgNameLabel.bottomAnchor, constant: padding),
-         descriptionTextView.bottomAnchor.constraint(equalTo: takeActionButton.topAnchor, constant: -padding),
-         descriptionTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding), descriptionTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding)].forEach {$0.isActive = true}
-    }
-    
-    private func constrainActivityTypeIconView() {
-        activityTypeIconView.translatesAutoresizingMaskIntoConstraints = false
-        [activityTypeIconView.topAnchor.constraint(equalTo: descriptionTextView.topAnchor),
-         activityTypeIconView.trailingAnchor.constraint(equalTo: descriptionTextView.trailingAnchor),
-         activityTypeIconView.heightAnchor.constraint(equalToConstant: 30)].forEach {$0.isActive = true}
-    }
-    
-    private func constrainTakeActionButton() {
-        takeActionButton.translatesAutoresizingMaskIntoConstraints = false
-        [takeActionButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: padding),
-         takeActionButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-         takeActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: (-4 * padding)),
-         takeActionButton.heightAnchor.constraint(equalToConstant: 30),
-         takeActionButton.widthAnchor.constraint(equalToConstant: 180)].forEach {$0.isActive = true}
-    }
-    
-    private func constrainMapView() {
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        [mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-         mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-         mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-         mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)].forEach {$0.isActive = true}
-    }
-    
-    private func constrainDismissButton() {
-        dismissButton.translatesAutoresizingMaskIntoConstraints = false
-        [dismissButton.leadingAnchor.constraint(equalTo: mapView.safeAreaLayoutGuide.leadingAnchor, constant: padding),
-         dismissButton.bottomAnchor.constraint(equalTo: mapView.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
-         dismissButton.heightAnchor.constraint(equalToConstant: 20),
-         dismissButton.widthAnchor.constraint(equalToConstant: 80)].forEach {$0.isActive = true}
-    }
-
-}
-
-extension ActionDetailViewController: MFMailComposeViewControllerDelegate {
-    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        switch result.rawValue {
-        case MFMailComposeResult.cancelled.rawValue:
-            print("Cancelled")
-        case MFMailComposeResult.saved.rawValue:
-            print("Saved")
-        case MFMailComposeResult.sent.rawValue:
-            print("Sent")
-        case MFMailComposeResult.failed.rawValue:
-            print("Error: \(String(describing: error?.localizedDescription))")
-        default:
-            break
+    extension ActionDetailViewController: MFMailComposeViewControllerDelegate {
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            switch result.rawValue {
+            case MFMailComposeResult.cancelled.rawValue:
+                print("Cancelled")
+            case MFMailComposeResult.saved.rawValue:
+                print("Saved")
+            case MFMailComposeResult.sent.rawValue:
+                print("Sent")
+            case MFMailComposeResult.failed.rawValue:
+                print("Error: \(String(describing: error?.localizedDescription))")
+            default:
+                break
+            }
+            controller.dismiss(animated: true, completion: nil)
         }
-        controller.dismiss(animated: true, completion: nil)
-    }
 }

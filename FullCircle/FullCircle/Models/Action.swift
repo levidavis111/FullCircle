d798f951870b397c0dd26494ac4ece919e5e43c5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 21:14:48 -0400

    adds isSaved property to Action model and allActions data

--- FullCircle/FullCircle/Models/Action.swift
+++ FullCircle/FullCircle/Models/Action.swift
@@ -16,15 +16,7 @@ struct Action: Codable {
     let location: String
     let actionURL: String
     let iconString: String//Stored in Assets folder
-    
-    static let allActions = [
-        Action(name: "BLM March", description: "March from Grand Army Plaza to City Hall to Protest Police Violence", organization: Organization.allOrganizations[0] , type: .hard, location: "New York City", actionURL: "NA", iconString: "TK"),
-        Action(name: "Email your rep about police reform bill", description: "Send a pre-filled email to your local representative asking them to support the recent police reform bill", organization: Organization.allOrganizations[1], type: .easy, location: "N/A", actionURL: "yourrep@government.org", iconString: "TK"),
-        Action(name: "Donate to BAJI", description: "Please click to button below to make a donation to our cause.", organization: Organization.allOrganizations[2], type: .easy, location: "N/A", actionURL: Organization.allOrganizations[2].donateURL, iconString: "TK"),
-        Action(name: "Call Governor Cuomo", description: "Call Governor Cuomo's office and demand that he release a full education-reform plan. 1-518-474-8390.", organization: Organization.allOrganizations[3], type: .medium, location: "N/A", actionURL: "N/A", iconString: "TK"),
-        Action(name: "Sign our petition", description: "We have a petition on change.org, please take the time to sign it and voice your support", organization: Organization.allOrganizations[4], type: .easy, location: "N/A", actionURL: "change.org", iconString: "TK")
-    ]
-    
+    var isSaved: Bool
 }
 
 enum ActionType: String, Codable {
@@ -32,3 +24,11 @@ enum ActionType: String, Codable {
     case medium
     case hard
 }
+
+var allActions = [
+    Action(name: "BLM March", description: "March from Grand Army Plaza to City Hall to Protest Police Violence", organization: Organization.allOrganizations[0], type: .hard, location: "New York City", actionURL: "NA", iconString: "TK", isSaved: false),
+    Action(name: "Email your rep about police reform bill", description: "Send a pre-filled email to your local representative asking them to support the recent police reform bill", organization: Organization.allOrganizations[1], type: .easy, location: "N/A", actionURL: "yourrep@government.org", iconString: "TK", isSaved: false),
+    Action(name: "Donate to BAJI", description: "Please click to button below to make a donation to our cause.", organization: Organization.allOrganizations[2], type: .easy, location: "N/A", actionURL: Organization.allOrganizations[2].donateURL, iconString: "TK", isSaved: false),
+    Action(name: "Call Governor Cuomo", description: "Call Governor Cuomo's office and demand that he release a full education-reform plan. 1-518-474-8390.", organization: Organization.allOrganizations[3], type: .medium, location: "N/A", actionURL: "N/A", iconString: "TK", isSaved: false),
+    Action(name: "Sign our petition", description: "We have a petition on change.org, please take the time to sign it and voice your support", organization: Organization.allOrganizations[4], type: .easy, location: "N/A", actionURL: "change.org", iconString: "TK", isSaved: false)
+]

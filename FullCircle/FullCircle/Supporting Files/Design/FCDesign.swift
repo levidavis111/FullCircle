d4aa87a26d4b54e212ecc0fc8d964888a71e28e5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-14 11:42:35 -0400

    adds custom fonts to FCDesign

--- FullCircle/FullCircle/Supporting Files/Design/FCDesign.swift
+++ FullCircle/FullCircle/Supporting Files/Design/FCDesign.swift
@@ -15,4 +15,9 @@ struct FCDesign {
     static let darkGrey = UIColor(displayP3Red: 194/255, green: 194/255, blue: 194/255, alpha: 1)
     static let medGrey = UIColor(displayP3Red: 229/255, green: 229/255, blue: 229/255, alpha: 1)
     static let lightGrey = UIColor(displayP3Red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+    
+    static let headerFont = UIFont(name: "WorkSans-Bold", size: 20)
+    static let subHeaderFont = UIFont(name: "WorkSans-Bold", size: 14)
+    static let bodyFont = UIFont(name: "WorkSans-Regular", size: 14)
+    static let captionFont = UIFont(name: "WorkSans-Regular", size: 10)
 }

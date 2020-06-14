f26fd19a615bb98a79d08d6673050fbceafdea07
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-06-13 21:55:04 -0400

    adds tableView delegate and datasource methods to profileVC+TableView

--- FullCircle/FullCircle/Controllers/ProfileViewController/ProfileViewController+TableView.swift
+++ FullCircle/FullCircle/Controllers/ProfileViewController/ProfileViewController+TableView.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 FullCircle. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        savedActions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension ProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let actionDetailVC = ActionDetailViewController()
+        let thisAction = savedActions[indexPath.row]
+        actionDetailVC.action = thisAction
+        print("Pressed")
+        navigationController?.pushViewController(actionDetailVC, animated: true)
+    }
+    
+}
